b7371f05af4108f7f8d98ee8dabbe90e363d00e7
Craig Hammond <80643440+CompleteSuspect@users.noreply.github.com>
2021-06-11 23:07:48 +0100

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -2,11 +2,14 @@ import random
 
 class Card():
 
-    def __init__(self, value, rank, suite , hidden = False):
-        self.value = value
-        self.suite = suite
+    def __init__(self, score, rank, suite , hidden = False):
+        self.score = score
         self.rank = rank
+        self.suite = suite
         self.hidden = hidden
+
+    def __str__(self):
+        return f'{self.score} {self.rank} {self.suite} {self.hidden}'
 
     def flip(self):
         if self.hidden == False:
@@ -14,14 +17,16 @@ class Card():
         else:
             self.hidden = False
 
-    def __str__(self):
-        return f'{self.value} {self.rank} {self.suite} {self.hidden}'
-
 class Deck():
 
     def __init__(self):
         self.deck_lst = []
 
+    def __iter__(self):
+        return iter(self.deck_lst)
+
+    def __getitem__(self, idx):
+        return self.deck_lst[idx]
 
     def card_52(self, reps = 1):
         card_attb =  {'rank':
@@ -38,17 +43,30 @@ class Deck():
         while rep < reps:
             for suite in range(4):
                 for rank in range(1, 14):
-                    self.deck_lst.append(Card(rank, card_attb['rank'][rank], card_attb['suite'][suite]))
+                    self.deck_lst.append(Card(rank, card_attb['rank'][rank], card_attb['suite'][suite])) #Card(score, rank, suite)
             rep += 1
 
     def shuffle(self):
         random.shuffle(self.deck_lst)
 
     def add_card(self, card):
-        self.deck_lst.append(card)
+        if type(card) == Card:
+            self.deck_lst.append(card)
 
-    def draw_card(self, index = 0):
-        return self.deck_lst.pop(index)
+    def draw_card(self, idx = 0):
+        try:
+            return self.deck_lst.pop(idx)
+        except IndexError as e:
+            print(e)
+
+    def get_score(self):
+        total = 0
+        for card in self.deck_lst:
+            total += card.score
+        return total
+
+    def get_qty(self):
+        return len(self.deck_lst)
 
 class Player():
 
@@ -56,25 +74,46 @@ class Player():
         self.hand = Deck()
         self.name = name
         self.money = 1000
+        self.bid = 0
+        self.split_bid = 0
 
-#everything below here is code to test the classes, game logic has not yet been implemented
+    def split_pair(self):
+        if self.bid != 0:
+            if self.bid <= self.money:
+                self.split_hand = Deck()
+                self.split_hand.add_card(self.hand[1])
+                self.split_bid = self.bid
+                self.money -= self.bid
+            else:
+                print(f'{self.name} does not have enough money to split a pair.')
+        else:
+            print(f'{self.name} has not yet set a bid.')
 
-shoe = Deck()
+    def set_bid(self, bid):
+        if bid <= self.money:
+            self.bid = bid
+            self.money -= bid
+        else:
+            print(f'{self.name} does not have enough money (${self.money}).')
 
-shoe.card_52()
+    def double_down(self):
+        if self.bid != 0:
+            if self.bid <= self.money:
+                self.money -= self.bid
+                self.bid += self.bid
+            else:
+                print(f'{self.name} does not have enough money ({self.money}) to double the bid of {self.bid}.')
+        else:
+            print(f'{self.name} has not yet set a bid.')
+#------------------------------------------------------------------------------------------
 
-player_lst = []
+zoe = Player('zoe')
 
-while True:
-    name = input('Please enter your name, enter blank name to continue: ')
-    if len(name) > 0:
-        player_lst.append(Player(name))
-    else:
-        break
+zoe.hand.add_card(Card(4, '4', 'Clubs'))
+zoe.hand.add_card(Card(5, '5', 'Hearts', hidden = True))
+zoe.set_bid(500)
 
-player_lst[0].hand.add_card(shoe.draw_card())
-player_lst[0].hand.add_card(shoe.draw_card())
-
-
-for card in player_lst[0].hand:
-    print(card)
+zoe.split_pair()
+print(zoe.hand[0])
+print(zoe.split_hand[0])
+print(zoe.split_bid)
